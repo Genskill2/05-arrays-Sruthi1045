14e11cc50c0fac12cbaaeb4c2e8c83d921acc4e7
Sruthi1045 <84717209+Sruthi1045@users.noreply.github.com>
2021-06-17 22:19:49 +0530

    my soln

--- solns.c
+++ solns.c
@@ -1,2 +1,90 @@
 /* Enter your solutions in this file */
 #include <stdio.h>
+#include<stlib.h>
+#include<assert.h>
+#include<math.h>
+int max(int a[],int n);
+{
+  int big,i;
+  big=a[0];
+  for(i=1;i<n;i++)
+  {
+    if(a[i]>big)
+    {
+      big=a[i];
+    }
+  }
+  return big;
+}
+int min(int a[],int n);
+{
+  int small,j;
+  small=a[0];
+  for(j=1;j<n;j++)
+  {
+    if(a[j]<small)
+    {
+      small=a[j];
+    }
+  }
+  return small;
+}
+float average(int a[],int n);
+{
+  int i=0,sum=0;
+  float p;
+  for(i=0;i<n;i++)
+  {
+    sum=sum+a[i];
+  }
+  p=(float)sum/n;
+  return p;
+}
+int mode(int a[],int n);
+{
+  int maxvalue=0,maxcount=0,i,j;
+  for(i=0;ii<n;i++)
+  {
+    int count=0;
+    for(j=0;j<n;j++)
+    {
+      if(a[j]==a[i])
+      {
+        count++;
+      }
+    }
+    if(count>maxcount)
+    {
+      maxcount=count;
+      maxvalue=a[i];
+    }
+  }
+  return maxvalue;
+}
+int factors(int n,int a[]);
+{
+  int count=0;
+  while(n%2==0)
+  {
+    a[count]=2;
+    count++;
+    n=n/2;
+  }
+  for(int i=3;i<=sqrt(n);i=i+2)
+  {
+    while(n%2==0)
+    {
+      a[count]=i;
+      count++;
+      n=n/i;
+    }
+  }
+  if(n>2)
+  {
+    a[count]=n;
+    count++;
+  }
+  return count;
+}
+    
+  
